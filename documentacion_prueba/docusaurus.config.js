1a7ae8b033597ac3396436894cb27cd554b9f5f1
AlejandroP02 <aparrilla@elpuig.xeill.net>
2024-03-21 12:22:05 +0100

    Docuementación

--- documentacion_prueba/docusaurus.config.js
+++ documentacion_prueba/docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
             items: [
               {
                 label: 'Instalate',
-                to: 'instalaion-node',
+                to: 'instalacion-node',
               },
             ],
           },
